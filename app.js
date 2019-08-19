921fee6bc69ad01b5d0621f8361942146314335b
Jitlada Supreeyaporn (AP) <jitlada.supreeyaporn@global.ntt>
2019-08-19 16:39:06 +0700

    connect to atlas with mongo client

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import {MongoClient} from 'mongodb'
 
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+const client = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+client.connect((err) => {
+  if (err){
+    console.log('error', err)
+  }
+  else{
+    console.log('connected to db')
+  }
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  client.db('test').collection('temp').find().toArray((err, docs) => {
+    console.log(docs)
+    res.status(200).send(JSON.stringify(docs))
+  })
+})
+
+app.listen(5000, () => console.log('Example app listening on port 5000!'))
