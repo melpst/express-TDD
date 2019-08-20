b7ca3a804b5abf915aca741a432ca3f8e4358d6a
Jitlada Supreeyaporn (AP) <jitlada.supreeyaporn@global.ntt>
2019-08-20 14:16:26 +0700

    can post data to mongodb

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ import {temp} from './models'
 const app = express()
 
 const client = mongoose.createConnection(process.env.MONGODB_URL,  { useNewUrlParser: true })
+const tempSchema = client.model('temp', temp)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  const tempSchema = client.model('temp', temp)
   tempSchema.find().exec((err, docs) => {
     if(err){
       res.status(500).send(err.message)
@@ -22,5 +22,20 @@ app.get('/', (req, res) => {
     }
   })
 })
+
+app.post('/', (req, res) => {
+  let newTemp = new tempSchema()
+  newTemp.text = "new temp"
+  newTemp.save((err, docs) => {
+    if(err){
+      res.status(500).send(err.message)
+    }
+    else{
+      console.log(JSON.stringify(docs))
+      res.status(200).send(JSON.stringify(docs))
+    }
+  })
+})
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
