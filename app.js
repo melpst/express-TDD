dbdbf7d052778321d0b757b3f56fb3816e185c62
melpst <jitlada.su@ku.th>
2019-08-22 15:09:41 +0700

    can post new user to collection

--- app.js
+++ app.js
@@ -14,4 +14,4 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/', router)
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,10 @@ import {DALController} from '../controller'
 
 const router = Router()
 const dal = new DALController()
+const userColl = mongoose.model('user', userModel)
 
 router.get('/', (req, res) => {
-  const coll = mongoose.model('user', userModel)
-  dal.findAll({'_id': '5d5d62c3ab56e83654c12883'}, {'_id': 'desc'}, 1, coll)
+  dal.findAll({'_id': '5d5d62c3ab56e83654c12883'}, {'_id': 'desc'}, 1, userColl)
   .then((docs) => {
     console.log(docs)
     res.status(200).send(docs)
@@ -20,9 +20,9 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  let newuser = new userModel()
-  newuser.text = "new user"
-  newuser.set('username', "hhoooooooooooooooooo")
+  const newuser = new userColl()
+  newuser.text = req.body.text
+  newuser.username = req.body.username
   newuser.save((err, docs) => {
     if(err){
       res.status(500).send(err.message)
