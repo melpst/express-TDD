04b61fe7c43f70cbef15437310a6c2d7e9216f42
melpst <jitlada.su@ku.th>
2019-09-11 12:17:24 +0700

    edit how to return mongo client and how to log

--- src/app.js
+++ src/app.js
@@ -7,13 +7,13 @@ import config from 'config'
 import router from './routes'
 import path from 'path'
 
-const client = mongoose.createConnection(process.env.MONGODB_URL+config.get('database'),  { useNewUrlParser: true , useFindAndModify: false})
-// .then((data) => {
-//     console.log('connected to db')
-//     module.exports.db = data
-//     return data
-// })
-// .catch((err) => console.log(err))
+const client = mongoose.connect(process.env.MONGODB_URL+config.get('database'),  { useNewUrlParser: true , useFindAndModify: false})
+.then((data) => {
+    logger.info('connected to db')
+    module.exports.db = data
+    return data
+})
+.catch((err) => logger.error(err))
 
 const app = express()
 
@@ -28,7 +28,7 @@ app.use('/', router)
 const server = app.listen(config.get('port'), () => {
     // debug(`server is running on port ${config.get('port')} and in ${config.get('name')} mode`);
     logger.info(`server is running on port ${config.get('port')} and in ${config.get('name')} mode`);
-    // console.log(client.client.s.url)
+    
     // console.log(`server is running on port ${config.get('port')} and in ${config.get('name')} mode`);
 })
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ import {Router} from 'express'
 import mongoose from 'mongoose'
 import userModel from '../models'
 import {DALController} from '../controller'
+import { logger } from '../logger';
 
 const router = Router()
 const dal = new DALController()
@@ -10,67 +11,73 @@ const userColl = mongoose.model('user', userModel)
 function findAll(filter, sort, limit, coll, res){
   dal.findAll(filter, sort, limit, coll)
   .then((docs) => {
-    // console.log(docs)
+    logger.debug(docs)
     res.status(200).send(docs)
   })
   .catch((err)=>{
-    console.log(err)
+    logger.error(err)
     res.status(500).send(err.message)
   })
 }
 
 router.get('/', (req, res) => {
+  logger.debug(`GET /user`)
   findAll({}, {'_id': 'desc'}, null, userColl, res)
 })
 
-router.get('/test', (req,res) => res.render('index'))
+router.get('/test', (req,res) => {
+  res.render('index')
+})
 
 router.get('/:id', (req, res) => {
   // findAll({'_id': req.params.id}, {'_id': 'desc'}, 1, userColl, res)
+  logger.debug(`GET /user/${req.params.id}`)
   dal.findAll({'_id': req.params.id}, {'_id': 'desc'}, 1, userColl)
   .then((docs) => {
-    console.log(docs)
+    logger.debug(docs)
     res.render('all', {users: docs})
   })
   .catch((err)=>{
-    console.log(err)
+    logger.error(err)
     res.status(500).send(err.message)
   })
 })
 
-
 router.put('/:id', (req, res) => {
+  logger.debug(`PUT /user/${req.params.id}`)
   dal.findOneAndUpdate({'_id': req.params.id}, {'name': 'test deprecate'}, {'new': true}, userColl)
   .then((docs) => {
-    console.log(docs)
+    logger.debug(docs)
     res.status(200).send(docs)
   })
   .catch((err)=>{
-    console.log(err)
+    logger.error(err)
     res.status(500).send(err.message)
   })
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
+  logger.debug(`POST /user`)
+  logger.debug(`req : ${req.body}`)
   const newUser = new userColl(req.body)
 
   dal.save(newUser)
   .then((docs) => {
-    console.log(docs)
+    logger.debug(docs)
     res.status(200).send(docs)
   })
   .catch((err)=>{
-    console.log(err)
+    logger.error(err)
     res.status(500).send(err.message)
   })
 })
 
 router.post('/getUserById', (req, res) => {
-  console.log(req.body)
+  logger.debug(`POST /getUserByID`)
+  logger.debug(`req : ${req.body.id}`)
   dal.findAll({'_id': req.body.id}, {'_id': 'desc'}, null, userColl)
   .then((docs) => {
-    console.log(docs)
+    logger.debug(docs)
     let users = '<table><thead><td>id</td><td>name</td><td>text</td></thead><tbody>'
     docs.forEach(doc => {
       users += '<tr><td>'+doc._id+'</td><td>'+doc.username+'</td><td>'+doc.text+'</td></tr>'
@@ -79,11 +86,9 @@ router.post('/getUserById', (req, res) => {
     res.status(200).send(users)
   })
   .catch((err)=>{
-    console.log(err)
+    logger.info(err)
     res.status(500).send(err.message)
   })
 })
-
-
 
 module.exports = router
