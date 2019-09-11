41173de5539aec6af23a73429ceab38500ba5aaa
melpst <jitlada.su@ku.th>
2019-09-11 12:16:41 +0700

    setting up log4js

--- src/logger.js
+++ src/logger.js
@@ -1,33 +1,31 @@
-const log4js = require('log4js');
+const log4js = require('log4js')
+import config from 'config'
 const current_datetime = new Date();
 var Log_config = log4js.configure({
-  
-    appenders: {
-      app: {
-        type: "file",
-        filename: "./logs/"+ current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear()+ " app.log" ,       
-        maxLogSize: 10485760,
-        numBackups: 3
-      
-      },
-      console:{ type: 'console'},
-      
-      apps: {
-        type: "logLevelFilter",
-        level: "INFO",
-        appender: "app"
-       
-      }
+  appenders: {
+    work: {
+      type: "file",
+      filename: "./logs/"+ current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear()+ " app.log" ,       
+      maxLogSize: 10485760,
+      numBackups: 3
     },
-    categories: {
-     default: { "appenders": ["apps","console"], "level": "DEBUG" }
-     
+    test: {
+      type: "file",
+      filename: "./logs/test/"+ current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear()+ " app.log" ,       
+      maxLogSize: 10485760,
+      numBackups: 3
+    },
+    console:{
+      type: "console"
     }
-
- 
+  },
+  categories: {
+    default: { "appenders": ["work","console"] , "level": "INFO" },
+    test: { "appenders": ["test"] , "level": "DEBUG" }
+  }
 });
 
-const logger = log4js.getLogger('');
+const logger = log4js.getLogger(config.get('logger'));
 
 export {
   logger,Log_config
