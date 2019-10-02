32c023da359e5791f0f21d928b438d52514013f7
melpst <jitlada.su@ku.th>
2019-10-02 14:27:15 +0700

    test timezone and jenkins

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ import {Router} from 'express'
 import {finalConfig} from '../../config'
 import user from './user'
 import activity from './activity'
+import { logger } from '../logger'
+import { DateTime } from 'luxon'
+import moment from 'moment-timezone'
 
 const router = Router()
 
@@ -9,5 +12,16 @@ router.use('/user', user)
 router.use('/activity', activity)
 
 router.get('/', (req,res) => res.send({'text': finalConfig.name}))
+
+router.get('/time', (req,res) => {
+    const today = {
+        date: null,
+        time: moment.tz(new Date(),'Asia/Bangkok')
+    }
+
+    logger.debug(today.time.toString())
+    logger.debug(typeof(today.time))
+    res.send({time: today.time.format()})
+})
 
 module.exports = router
