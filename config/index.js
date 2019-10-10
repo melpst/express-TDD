4fb10e76277252ea4a5c60ea5499000626fe9797
melpst <jitlada.su@ku.th>
2019-10-10 15:53:01 +0700

    show all config

--- config/index.js
+++ config/index.js
@@ -4,11 +4,12 @@ const environment = process.env.NODE_ENV || 'development';
 const environmentConfig = config[environment];
 
 environmentConfig.database.url = process.env.MONGODB_URL+environmentConfig.database.url
-console.log("finalconfig", environmentConfig.database.url)
 
 const finalConfig = {
     ...environmentConfig,
     ELASTIC_URL: process.env.ELASTIC_URL
 }
+
+console.log(finalConfig)
 
 export {finalConfig}
